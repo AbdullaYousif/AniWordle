24255b24ce717fee8eee230aa4f595de0157748e
malkh1 <mohammadalkhaledi2@gmail.com>
2024-09-03 16:43:54 -0400

    Implemented Character equality method

--- AnimeWordle/demo/src/main/java/com/example/demo/model/Character.java
+++ AnimeWordle/demo/src/main/java/com/example/demo/model/Character.java
@@ -12,7 +12,7 @@ public class Character{
     private String hairColor;
     private String specialAbilities;
     private String affiliation;
-
+    
     public Character(String nameOfSeries, String name, boolean gender, String hairColor, String specialAbilities, String affiliation) {
         this.nameOfSeries = nameOfSeries;
         this.name = name;
@@ -24,6 +24,62 @@ public class Character{
 
     public Character(){
         this(" ", " ", true, " ", " ", " ");
+    }
+    
+    /**
+     * Compares two objects if they are equal.
+     * If the objects being compared have the same reference, it's the same object
+     * If the objects being compared are both character objects, then comparison
+     * between each field is done and a message is printed in console accordingly
+     * 
+     * otherwise, the objects are not equal
+     * @param anObject Object to be compared
+     * @return true or false, but value not being used
+     */
+    @Override
+    public boolean equals(Object anObject) {
+        if(this == anObject) {
+            System.out.println("this is the same object");
+            return true;
+        }
+        else if(anObject instanceof Character) {
+            if(nameOfSeries.equalsIgnoreCase(((Character) anObject).getNameOfSeries())) {
+                System.out.println("Correct name of series");
+            }
+            else {
+                System.out.println("Wrong name of series");
+            }
+            
+            if(gender == ((Character) anObject).getGender()) {
+                System.out.println("Correct gender");
+            }
+            else {
+                System.out.println("Wrong gender");
+            }
+            
+            if(hairColor.equalsIgnoreCase(((Character) anObject).getHairColor())) {
+                System.out.println("Correct hair color");
+            }
+            else {
+                System.out.println("Wrong hair color");
+            }
+            
+            if(specialAbilities.equalsIgnoreCase(((Character) anObject).getSpecialAbilities())) {
+                System.out.println("Correct special ability");
+            }
+            else {
+                System.out.println("Wrong special ability");
+            }
+            
+            if(affiliation.equalsIgnoreCase(((Character) anObject).getAffiliation())) {
+                System.out.println("Correct affiliation");
+            }
+            else {
+                System.out.println("Wrong affiliation");
+            }
+        }
+        
+        return false;
     }
     
     public Long getId(){
